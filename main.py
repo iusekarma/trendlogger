1744ee4c96774387a2beeff954db561f6c209628
Ayush Vishwakarma <103050418+iusekarma@users.noreply.github.com>
2022-09-29 16:39:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from pytrends.request import TrendReq
 from datetime import datetime
 
-#pytrend = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
+#get the top 5 thrends
 pytrend = TrendReq()
-
 trends = []
-
 trendingtoday = pytrend.today_searches(pn='IN')
 for i in trendingtoday[:5]:
     trends.append(i.split('=')[1][:-5].replace('+',' '))
 
+#log it in trend.csv
 with open('trend.csv','a') as file:
     file.writelines(f'{datetime.now().__str__()},{",".join(trends)}\n')
 
@@ -20,7 +19,7 @@ with open('README.md','r') as file:
 
 first_section = readme[:readme.index('<!-- Last Trends -->')]
 last_section =  readme[readme.index('<!-- Requirements -->'):]
-mid_section = "\n<!-- Last Trends -->\n### Last Trends\n* "+'\n* '.join(trends)+"\n> "+str(datetime.now())+"\n\n"
+mid_section = "<!-- Last Trends -->\n### Last Trends\n* "+'\n* '.join(trends)+"\n> "+str(datetime.now())+"\n\n"
 
 with open('README.md','w') as file:
     file.write(first_section+mid_section+last_section)
